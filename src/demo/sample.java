0bf178edbe113fc6e83bf363cd0711357d2b09e2
mano3590 <jjmano2008@gmail.com>
2019-03-19 00:18:54 +0530

    My Task1

--- src/demo/sample.java
+++ src/demo/sample.java
@@ -1,5 +1,22 @@
 package demo;
 
-public class sample {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+
+public class sample {
+	public static void main(String[] args) {
+		
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\prasanth\\Downloads\\eclipse-jee-2018-12-R-win32-x86_64\\eclipse\\prasanth\\Task1\\easy\\driver\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com");
+	WebElement src = driver.findElement(By.id("email"));
+	src.sendKeys("psyan007@gmail.com");
+	WebElement desc = driver.findElement(By.id("pass"));
+	desc.sendKeys("12345789");
+	
+
+}
 }
